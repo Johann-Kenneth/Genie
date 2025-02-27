d88e74363160cc8ecbe214ffb6e7db840665eaa5
Johann-Kenneth <johannkenneth.ad.mfedu@gmail.com>
2025-02-27 16:17:47 +0530

    mongo connection changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,6 +6,8 @@ dotenv.config();
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
       // Removed deprecated options
     });
     console.log('MongoDB connected');
